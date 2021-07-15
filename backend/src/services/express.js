a756de942d5fd4c0178d590f1d349c50c0c94a05
Mustafa Emin Bosnalı <mustafaeminbosnali@hotmail.com>
2021-07-15 22:36:17 +0300

    cpmfg updated

--- backend/src/services/express.js
+++ backend/src/services/express.js
@@ -26,7 +26,7 @@ app.use(errorHandler.handleNotFound);
 app.use(errorHandler.handleError);
 app.use(function (req, res, next) {
   // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "*");
 
   // Request methods you wish to allow
   res.setHeader(
